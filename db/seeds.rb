c219e09f00f4f7367302c3f7e3606841b571a1e4
Kylan Plunk <kaplunk1@memphis.edu>
2024-04-30 03:52:35 -0500

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Product.create(name: "Baseball Bat", price: 9.99, description: "Used for hitting baseballs usually.")
+Product.create(name: "Basketball Bat", price: 19.99, description: "I dunno.")
+Product.create(name: "Fruit Bat", price: 29.99, description: "Used for eating fruit usually.")
